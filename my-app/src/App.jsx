319a8325c253205ddfe622d82aa034e75703e762
Ali Hamza <141618199+AliHamza003@users.noreply.github.com>
2024-03-29 20:39:17 +0500

    c

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App=()=>{
+  
+
+  return (
+    <>
+      <h1>Currency Converter</h1>
+      <div className="main">
+      <h3>Exchange Rate</h3>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
